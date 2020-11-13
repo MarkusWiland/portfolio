0a38685f7addd1bb587cfbe8f7d90f24d87ef77c
Markus Wiland <markus.wiland@outlook.com>
2020-11-13 17:04:50 +0100

    working on the design

--- components/Header.js
+++ components/Header.js
@@ -8,7 +8,7 @@ export default function Header() {
                <h3 className={styles.navbar__logo}>Markus <span className={styles.navbar__h2tag}>Wiland</span></h3>
                <nav className={styles.navbar__nav}> 
                    <ul className={styles.navbar__ul}>
-                       <Link href="/"><li className={styles.navbar__li}><a>Starta</a></li></Link>
+                       <Link href="/"><li className={styles.navbar__li}><a>Start</a></li></Link>
                        <Link href="/blog"><li className={styles.navbar__li}><a>Blogg</a></li></Link>
                        <Link href="/portfolio"><li className={styles.navbar__li}><a>Portfolio</a></li></Link>
                        <Link href="/about"><li className={styles.navbar__li}><a>Om Mig</a></li></Link>
